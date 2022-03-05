bcac665f42f9330eb1f77893ecb7bddd126690a2
Tyler Olexa <tolexa@student.cscc.edu>
2022-03-05 18:10:03 -0500

    Initial commit for Lab 7

---
+++ src/edu/cscc/Main.java
@@ -0,0 +1,58 @@
+package edu.cscc;
+
+import java.util.*;
+
+/**
+ * Main for Rock, Paper, Scissors, Lizard, Spock
+ */
+public class Main {
+
+    private static Scanner input = new Scanner(System.in);
+
+    /**
+     * Main method for running Rock, Paper, Scissors, Lizard, Spock
+     * @param args run-time arguments
+     */
+    public static void main(String[] args) {
+        String h_pick;
+        String c_pick;
+        String answer;
+        boolean isValid;
+
+        do {
+            System.out.println("Let's play rock, paper, scissors, lizard, spock");
+            do {
+                System.out.print("Enter your choice: ");
+                h_pick = input.nextLine().trim();
+                isValid = RPSLSpock.isValidPick(h_pick);
+                if (!isValid) {
+                    System.out.println(h_pick + " is not a valid choice");
+                }
+            } while (!isValid);
+
+            c_pick = RPSLSpock.generatePick();
+            System.out.print("Computer picked " + c_pick + "  ");
+
+            if (c_pick.equalsIgnoreCase(h_pick)) {
+                System.out.println("Tie!");
+            } else if (RPSLSpock.isComputerWin(c_pick, h_pick)) {
+                System.out.println("Computer wins!");
+            } else {
+                System.out.println("You win!");
+            }
+
+            System.out.print("Play again ('y' or 'n'): ");
+            answer = input.nextLine().trim();
+        } while ("Y".equalsIgnoreCase(answer));
+        System.out.println("Live long and prosper!");
+    }
+}
+
+/*
+ * Copyright Notice: Columbus State Community College and its employees hold the copyright for this course material. This material is
+ * made available to students for their personal use only and may not be distributed for commercial purposes without the College’s
+ * express written consent. Uploading this copyrighted material to "tutoring" or other non-Columbus State web sites is prohibited and
+ * may result in referral to the Office of Student Conduct and disciplinary action up to and including dismissal.
+ *
+ * Course ID: 40A9B085A383430396E7BACF467DE6E0
+ */
---
+++ src/edu/cscc/RPSLSpock.java
@@ -0,0 +1,84 @@
+package edu.cscc;
+import java.util.Random;
+
+/**
+ * Rock, Paper, Scissors, Lizard, Spock game lab.
+ * @author Tyler Olexa, March 5th 2022, for CSCI 2467.
+ */
+public class RPSLSpock {
+
+    static Random rand = new Random(System.currentTimeMillis());
+
+    public static final String ROCK = "rock";
+    public static final String PAPER = "paper";
+    public static final String SCISSORS = "scissors";
+    public static final String LIZARD = "lizard";
+    public static final String SPOCK = "spock";
+
+    /**
+     * isValidPick method
+     * @param pick
+     * @return a boolean value that indicates whether or not the pick was valid.
+     */
+    public static boolean isValidPick(String pick) {
+        if (pick == null) {
+            return false;
+        }
+        pick = pick.trim();
+        return (ROCK.equalsIgnoreCase(pick) ||
+                PAPER.equalsIgnoreCase(pick) ||
+                SCISSORS.equalsIgnoreCase(pick) ||
+                LIZARD.equalsIgnoreCase(pick) ||
+                SPOCK.equalsIgnoreCase(pick));
+    }
+
+    /**
+     * generatePick method
+     * @return a string that signifies the pick
+     */
+    public static String generatePick() {
+        String pick = null;
+        switch (rand.nextInt(5)) {
+            case 0:
+                pick = ROCK;
+                break;
+            case 1:
+                pick = PAPER;
+                break;
+            case 2:
+                pick = SCISSORS;
+                break;
+            case 3:
+                pick = LIZARD;
+                break;
+            case 4:
+                pick = SPOCK;
+                break;
+        }
+        return pick;
+    }
+
+    /**
+     * isComputerWin method
+     * @param c_pick the computer's pick
+     * @param h_pick the human's pick
+     * @return
+     */
+    public static boolean isComputerWin(String c_pick,String h_pick) {
+        h_pick = h_pick.toLowerCase().trim();
+        return ((ROCK.equals(c_pick) && (SCISSORS.equals(h_pick) || LIZARD.equals(h_pick))) ||
+                (PAPER.equals(c_pick) && (ROCK.equals(h_pick) || SPOCK.equals(h_pick))) ||
+                (SCISSORS.equals(c_pick) && (PAPER.equals(h_pick) || LIZARD.equals(h_pick))) ||
+                (LIZARD.equals(c_pick) && (PAPER.equals(h_pick) || SPOCK.equals(h_pick))) ||
+                (SPOCK.equals(c_pick) && (ROCK.equals(h_pick) || SCISSORS.equals(h_pick))));
+    }
+}
+
+/*
+ * Copyright Notice: Columbus State Community College and its employees hold the copyright for this course material. This material is
+ * made available to students for their personal use only and may not be distributed for commercial purposes without the College’s
+ * express written consent. Uploading this copyrighted material to "tutoring" or other non-Columbus State web sites is prohibited and
+ * may result in referral to the Office of Student Conduct and disciplinary action up to and including dismissal.
+ *
+ * Course ID: 40A9B085A383430396E7BACF467DE6E0
+ */
